4a9fd7c1d4568f2107c644ffc3394b1273e48be8
Sanu Islam <dropnow.sanu@gmail.com>
2023-12-17 20:47:18 +0600

    update site menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-2xl justify-center font-bold text-rose-600'>
+    <div className='max-w-7xl mx-auto h-full text-2xl justify-center font-bold text-rose-600 bg-slate-900'>
       Saidpur News
     </div>
   )
